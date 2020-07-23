4f0abb01e349170e920c0cacd00141698db2ece1
emymaria <emymariajune@gmail.com>
2020-07-23 17:35:31 +0530

    JPA RestServices development completed

---
+++ src/main/java/com/stacksimplify/restservices/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.stacksimplify.restservices.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.stacksimplify.restservices.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+	Optional<User> findUserByUsername(String username);
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101,'qqq@gmail.com','qqq','www','eee','q1','qwe')
+insert into user values(102,'rrr@gmail.com','rrr','ttt','yyy','r2','rty')
+insert into user values(103,'uuu@gmail.com','uuu','iii','ppp','u1','uip')
